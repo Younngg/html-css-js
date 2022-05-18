931328283953025289b2433fde017220f1979f29
Jiyeong Jeong <98656282+Younngg@users.noreply.github.com>
2022-05-18 17:21:01 +0900

    0518

---
+++ 0518/js1.js
@@ -0,0 +1,34 @@
+
+
+let num = 50;
+let str = '자바스크립트';
+let num1 = '2022';
+let value1 = '2022' + 2022;
+console.log(value1);
+
+let str1 = 2022;
+let str2 = `안녕 ${str1}년`;
+console.log(str2);
+
+console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
+
+
+let userName = ['a', 'b'];
+let user = [
+  {
+    name: 'a',
+    age: 20
+  },
+  {
+    name: 'b',
+    age:30
+  }
+];
+
+console.log(userName[0]);
+console.log(user[1]);
